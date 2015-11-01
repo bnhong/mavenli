a7bbf6c6fce1e2cf056e8ef400b66ffec0543e27
matty4 <matthew.k.yuan@gmail.com>
2015-11-01 13:13:18 -0800

    Marking our turf

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -14,12 +13,6 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-

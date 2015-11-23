c3c35273c2163389be668d7100cce73197a3f5c4
matty4 <matthew.k.yuan@gmail.com>
2015-11-22 21:31:40 -0800

    Added some stuff and fixed the search bar.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +12,9 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+app.get('/partner', function(request, response ) {
+  response.render('pages/partner');
 });
 
 app.listen(app.get('port'), function() {

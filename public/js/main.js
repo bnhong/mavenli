16d5b54099454c6af64304ce8b9b0c7e690b7ae8
Bunreth Nhong <bunrethnhong@gmail.com>
2016-01-21 23:57:58 -0800

    * Update main page to use Angular. * Included locations.json file to simulate database. * Need to refactor main ng-app.

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+angular.module('mainApp', [])
+
+.controller('CitiesController', ['$scope', '$http', function($scope, $http) {
+  $http.get('../json/locations.json')
+    .success(function(data){
+      $scope.destinations = data;
+      console.log($scope.destinations);
+    });
+}])
+
+.controller('RsvpForm', ['$scope', '$http', function($scope, $http) {
+  $scope.list = [];
+  $scope.rsvpList = [];
+
+  $scope.text;
+  $scope.checkin;
+  $scope.checkout;
+  $scope.guests;
+
+
+  $scope.submit = function() {
+    if ($scope.text) {
+      $scope.list.push(this.text);
+      $scope.text = '';
+      $scope.list.push(this.checkin);
+      $scope.checkin = '';
+      $scope.list.push(this.checkout);
+      $scope.checkout = '';
+      $scope.list.push(this.guests);
+      $scope.guests = '';
+    }
+  };
+
+  $scope.rsvp = function() {
+    $scope.rsvpList.push({
+      'searched': $scope.text,
+      'checkin': $scope.checkin,
+      'checkout': $scope.checkout,
+      'guests': $scope.guests
+    });
+
+    var dataObj = {
+      searched : $scope.text,
+      checkin : $scope.checkin,
+      checkout : $scope.checkout,
+      guests : $scope.guests
+    };
+
+    $http.post('../json/temp.json', dataObj)
+      .success(function(data, status, headers, config) {
+        $scope.message = data;
+      });
+
+    $scope.text = '';
+    $scope.checkin = '';
+    $scope.checkout = '';
+    $scope.guests = '';
+  };
+}]);
